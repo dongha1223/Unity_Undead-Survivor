f27eaf9b40095d293d69425b7d4e666c4c3a2d1f
dongha1223 <80883907+dongha1223@users.noreply.github.com>
2023-11-26 20:28:54 +0900

    2023 11 26

--- Assets/Undead Survivor/Scripts/AchiveManager.cs
+++ Assets/Undead Survivor/Scripts/AchiveManager.cs
@@ -7,13 +7,16 @@ public class AchiveManager : MonoBehaviour
 {
     public GameObject[] lockCharacter;
     public GameObject[] unlockCharacter;
+    public GameObject uiNotice;
 
     enum Achive { UnlockPotato, UnlockBean }
     Achive[] achives;
+    WaitForSecondsRealtime wait;
 
     void Awake()
     {
         achives = (Achive[])Enum.GetValues(typeof(Achive));
+        wait = new WaitForSecondsRealtime(5);
 
         if (!PlayerPrefs.HasKey("MyData")) {
             Init();
@@ -26,7 +29,7 @@ public class AchiveManager : MonoBehaviour
 
         foreach (Achive achive in achives)
         {
-            PlayerPrefs.SetInt(achive.ToString(), 1);
+            PlayerPrefs.SetInt(achive.ToString(), 0);
         } 
     }
 
@@ -44,5 +47,49 @@ public class AchiveManager : MonoBehaviour
             lockCharacter[Index].SetActive(!isUnlock);
             unlockCharacter[Index].SetActive(isUnlock);
         }
+    }
+
+    void LateUpdate()
+    {
+        foreach (Achive achive in achives)
+        {
+            CheckAchive(achive);
+        }
+    }
+
+    void CheckAchive(Achive achive)
+    {
+        bool isAchive = false;
+
+        switch (achive)
+        {
+            case Achive.UnlockPotato:
+                isAchive = GameManager.instance.kill >= 10;
+                break;
+            case Achive.UnlockBean:
+                isAchive = GameManager.instance.gameTime == GameManager.instance.maxGameTime;
+                break;
+        }
+
+        if (isAchive && PlayerPrefs.GetInt(achive.ToString()) == 0) {
+            PlayerPrefs.SetInt(achive.ToString(), 1);
+
+            for (int index = 0; index < uiNotice.transform.childCount; index++)
+            {
+                bool isActive = index == (int)achive;
+                uiNotice.transform.GetChild(index).gameObject.SetActive(isActive);
+            }
+
+            StartCoroutine(NoticeRoutine());
+        } 
+    }
+
+    IEnumerator NoticeRoutine()
+    {
+        uiNotice.SetActive(true);
+
+        yield return wait;
+
+        uiNotice.SetActive(false);
     }
 }
